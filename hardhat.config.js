4bc16c437a7ed49a732f5c99cc59159f6eede22f
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2022-06-18 15:46:38 +0530

    tests update and bug fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-ganache");
+// require("@nomiclabs/hardhat-ganache");
 require("@nomiclabs/hardhat-web3");
 require("dotenv").config();
 const {POLYGON_MUMBAI_RPC_PROVIDER, POLYGON_MAINNET_PROVIDER, PRIVATE_KEY, POLYGON_API_KEY} = process.env;
@@ -13,6 +13,11 @@ module.exports = {
     matic: {
       url: POLYGON_MUMBAI_RPC_PROVIDER,
       accounts: [PRIVATE_KEY]
+    },
+    ganache:{
+      url: "http://127.0.0.1:8545",
+      chainId:1337,
+      accounts: ["0xe8a028160e51b08c80c450e1e90f0e8deef6362c97d3b5b44488ce5a5fbf6110"]
     }
   },
   etherscan: {
--- test/atokenCrowdsale.test.js
+++ test/atokenCrowdsale.test.js
@@ -1,23 +1,27 @@
 // import ether from './helpers/ether';
-const { expect } = require('chai');
+const { expect, assert } = require('chai');
+require('chai').should();
 const { ethers } = require('hardhat');
 const _name = "atoken";
 const _symbol = "ATK";
 
 const _rate = 500;
-const _wallet = "0xdD2FD4581271e230360230F9337D5c0430Bf44C0";
+const _wallet = "0xbc396Bb6B4680C4eC6146cF30AC0573409114D35";
 
 var atokenContract=null;
 var crowdsale = null;
 
 var BN = web3.utils.BN;
 
+var investor1 = "0x03Da047A80a8fdd571162c0c741714934CB9706a";
+var investor2 = "0xbc396Bb6B4680C4eC6146cF30AC0573409114D35";
+
 before(async ()  =>{
     const atoken = await ethers.getContractFactory("atoken");
     atokenContract = await atoken.deploy(_name, _symbol);
 
     const crowdsaleT = await ethers.getContractFactory("atokenCrowdsale");
-    crowdsale = await crowdsaleT.deploy(_rate, _wallet, atokenContract.address);
+    crowdsale = await crowdsaleT.deploy(_rate, _wallet, atokenContract.address, ethers.utils.parseEther("100"));
 })
 
 describe("crowdsale tests", function(){
@@ -49,7 +53,7 @@ describe('minted crowdsale', function() {
     it('mints tokens after purchase', async function() {
       const originalTotalSupply = await atokenContract.totalSupply();
       console.log(web3.utils.toWei("1", 'ether'));
-      const signer = await ethers.provider.getSigner("0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199");
+      const signer = await ethers.provider.getSigner("0xb2EFFe792146201175100196Ac13F79763ca9bF1");
       console.log(await signer.getBalance());
       await signer.sendTransaction({to:crowdsale.address, value:ethers.utils.parseEther("1")});
     //   await crowdsale.sendTransaction({ value: web3.utils.toWei("1", 'ether'), from: "0xFE9F5c37D394aDa800d6B76222C871aCbdA3d660" });
@@ -60,29 +64,32 @@ describe('minted crowdsale', function() {
 
 describe('accepting payments', function() {
     it('should accept payments', async function() {
-        const value = ether(1);
-        const purchaser = investor2;
-        await crowdsale.sendTransaction({ value: value, from: "0xFE9F5c37D394aDa800d6B76222C871aCbdA3d660" }).should.be.fulfilled;
-        await crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
+        const value = ethers.utils.parseEther("1");
+        const purchaser = "0xbc396Bb6B4680C4eC6146cF30AC0573409114D35";
+        const signer = await ethers.provider.getSigner("0xb2EFFe792146201175100196Ac13F79763ca9bF1");
+        await signer.sendTransaction({to:crowdsale.address, value:ethers.utils.parseEther("1")});
+        // await crowdsale.sendTransaction({ value: value, from: "0x03Da047A80a8fdd571162c0c741714934CB9706a" }).should.be.fulfilled;
+        await crowdsale.buyTokens(purchaser, { value: value, from: purchaser }).should.be.fulfilled;
+        // await assert.isFulfilled(crowdsale.buyTokens(purchaser, { value: value, from: purchaser }))
     });
 });
 
 describe('buyTokens()', function() {
     describe('when the contribution is less than the minimum cap', function() {
       it('rejects the transaction', async function() {
-        const value = atokenCrowdsale.investorMinCap - 1;
-        await atokenCrowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.rejectedWith(EVMRevert);
+        const value = crowdsale.investorMinCap - 1;
+        await crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.revertedWith("revert");
       });
     });
 
     describe('when the investor has already met the minimum cap', function() {
       it('allows the investor to contribute below the minimum cap', async function() {
         // First contribution is valid
-        const value1 = ether(1);
-        await atokenCrowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+        const value1 = ethers.utils.parseEther("1");
+        await crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
         // Second contribution is less than investor cap
         const value2 = 1; // wei
-        await atokenCrowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.fulfilled;
+        await expect(crowdsale.buyTokens(investor1, { value: value2, from: investor1 })).to.be.fulfilled;
       });
     });
   });
@@ -90,19 +97,19 @@ describe('buyTokens()', function() {
   describe('when the total contributions exceed the investor hard cap', function () {
     it('rejects the transaction', async function () {
       // First contribution is in valid range
-      const value1 = ether(2);
-      await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+      const value1 = ethers.utils.parseEther("2");
+      await crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
       // Second contribution sends total contributions over investor hard cap
-      const value2 = ether(49);
-      await this.crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.rejectedWith(EVMRevert);
+      const value2 = ethers.utils.parseEther("49");
+      await crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.revertedWith("revert");
     });
   });
 
   describe('when the contribution is within the valid range', function () {
-    const value = ether(2);
+    const value = ethers.utils.parseEther("2");
     it('succeeds & updates the contribution amount', async function () {
-      await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.fulfilled;
-      const contribution = await this.crowdsale.getUserContribution(investor2);
+      await crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.fulfilled;
+      const contribution = await crowdsale.getUserContribution(investor2);
       contribution.should.be.bignumber.equal(value);
     });
   });
