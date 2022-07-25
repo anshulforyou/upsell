c1bf40ce523a25ce60967d696e034a34e1f58c3d
ASMZJ <108855950+ASMZJ@users.noreply.github.com>
2022-07-25 18:46:54 +0530

    Updated Token contract

--- contracts/UPsellToken.sol
+++ contracts/UPsellToken.sol
@@ -1,40 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.7;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract UPsellToken is ERC20PresetMinterPauser, Ownable{
-    // uint256 private tokenSupply = 0;
-    constructor() ERC20PresetMinterPauser("UPsellToken", "UPSELL"){
+contract UPsellToken is ERC20PresetMinterPauser, Ownable {
+    uint256 private _cap;
+
+    constructor(uint256 cap_) ERC20PresetMinterPauser("UPsellToken", "UPSELL") {
+        require(cap_ > 0, "ERC20Capped: cap is 0");
+        _cap = cap_;
         _mint(msg.sender, 15000*10**uint256(decimals()));
-        // tokenSupply+=10000*10**uint256(decimals);
-        // return tokenSupply();
     }
 
-    function getMoreTokens(uint quantity) public onlyOwner{
-        _mint(msg.sender, quantity*10**uint256(decimals()));
-        // tokenSupply+=quantity;
-        // return tokenSupply();
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function cap() public view virtual returns (uint256) {
+        return _cap;    
     }
 
-    function burnTokens(address account, uint256 amount) public onlyOwner{
-        _burn(account, amount);
-        // tokenSupply-=amount;
-        // return tokenSupply();
-    }
-
-    function giveRewards(address rewardee, uint256 amount) public onlyOwner{
-        _mint(rewardee, amount);
-        // tokenSupply+=amount;
-        // return tokenSupply();
-    }
-
-    function pauseTransfer() public onlyOwner{
-        _pause();
-    }
-
-    function resumeTransfer() public onlyOwner{
-        _unpause();
+    /**
+     * @dev See {ERC20-_mint}.
+     */
+    function _mint(address account, uint256 amount) internal virtual override {
+        require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+        super._mint(account, amount);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,18 +3,23 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 // require("@nomiclabs/hardhat-ganache");
 require("@nomiclabs/hardhat-web3");
+require("hardhat-gas-reporter");
 require("dotenv").config();
-const {POLYGON_MUMBAI_RPC_PROVIDER, POLYGON_MAINNET_PROVIDER, PRIVATE_KEY, POLYGON_API_KEY} = process.env;
+const {POLYGON_MUMBAI_RPC_PROVIDER, POLYGON_MAINNET_RPC_PROVIDER, PRIVATE_KEY, POLYGON_API_KEY} = process.env;
 module.exports = {
-  defaultNetwork: "matic",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
     },
-    matic: {
+    mumbai: {
       url: POLYGON_MUMBAI_RPC_PROVIDER,
       accounts: [PRIVATE_KEY]
     },
-    ganache:{
+    matic: {
+      url: POLYGON_MAINNET_RPC_PROVIDER,
+      accounts: [PRIVATE_KEY]
+    },
+    localhost:{
       url: "http://127.0.0.1:8545",
       chainId:1337,
       accounts: ["0xe8a028160e51b08c80c450e1e90f0e8deef6362c97d3b5b44488ce5a5fbf6110"]
@@ -24,13 +29,26 @@ module.exports = {
     apiKey: POLYGON_API_KEY,
   },
   solidity: {
-    version: "0.8.7",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800
+          }
+        }
+      },
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 800
+          }
+        }
       }
-    }
+    ]
   },
   paths: {
     sources: "./contracts",
